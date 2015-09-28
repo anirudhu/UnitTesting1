cbcc918f42fb03062ce72ad8d2fb49814981a61b
Anirudh Upadhyayula <aupadhyayula@worcester.edu>
2015-09-27 20:29:03 -0400

    Unit-Testing Version 1

--- src/edu/worcester/cs/kwurst/DegreeAudit.java
+++ src/edu/worcester/cs/kwurst/DegreeAudit.java
@@ -20,50 +20,53 @@ package edu.worcester.cs.kwurst;
 /**
  * Creates student objects and calls methods on them.
  * 
- * @author Karl R. Wurst 
+ * @author Karl R. Wurst
  * @version JUnit Lab
  */
 public class DegreeAudit
 {
-    /**
-     * Creates Student objects and calls methods on them to test the methods.
-     * 
-     * @param args commandline arguments (not used)
-     */
-    public static void main(String[] args) {
-        // Check static constant representing the number of credits required to graduate.
-        System.out.println("credits to graduate = " + Student.REQUIRED_CR);
-        // Initial check of number of student created.
-        System.out.println("Number of students " + Student.getStudentCount());
-        
-        // Create a student.
-        Student s1 = new Student("Jane", "Smith");
-        // Print the student's ID.
-        System.out.println("ID " + s1.getId());
-        // Check count of created students.
-        System.out.println("Number of students " + Student.getStudentCount());
-
-        // Set some characteristics of the student.
-        s1.setGpa(2.5);
-        s1.setCurrentEarnedCr(110);
-        // Check number of credits remaining.
-        System.out.println(s1.getCurrentRemainingCr());
-        // Change credits.
-        s1.setCurrentEarnedCr(120);
-        // Check number of credits remaining.
-        System.out.println(s1.getCurrentRemainingCr());
-        
-        // Set LASC and major requirement to be complete.
-        s1.setMajorComplete(true);
-        s1.setLascComplete(true);
-        // Is the student ready to graduate?
-        System.out.println("Ready to graduate: " + s1.readyToGraduate());
-        
-        // Create another student.
-        Student s2 = new Student("John", "Smith");
-        // Check that the ID number has gone up.
-        System.out.println("ID " + s2.getId());
-        //Check that the number of students has gone up.
-        System.out.println("Number of students " + Student.getStudentCount());
-    }
+	/**
+	 * Creates Student objects and calls methods on them to test the methods.
+	 * 
+	 * @param args
+	 *          commandline arguments (not used)
+	 */
+	public static void main(String[] args)
+	{
+		// Check static constant representing the number of credits required to
+		// graduate.
+		System.out.println("credits to graduate = " + Student.REQUIRED_CR);
+		// Initial check of number of student created.
+		System.out.println("Number of students " + Student.getStudentCount());
+		
+		// Create a student.
+		Student s1 = new Student("Jane", "Smith");
+		// Print the student's ID.
+		System.out.println("ID " + s1.getId());
+		// Check count of created students.
+		System.out.println("Number of students " + Student.getStudentCount());
+		
+		// Set some characteristics of the student.
+		s1.setGpa(2.5);
+		s1.setCurrentEarnedCr(110);
+		// Check number of credits remaining.
+		System.out.println(s1.getCurrentRemainingCr());
+		// Change credits.
+		s1.setCurrentEarnedCr(120);
+		// Check number of credits remaining.
+		System.out.println(s1.getCurrentRemainingCr());
+		
+		// Set LASC and major requirement to be complete.
+		s1.setMajorComplete(true);
+		s1.setLascComplete(true);
+		// Is the student ready to graduate?
+		System.out.println("Ready to graduate: " + s1.readyToGraduate());
+		
+		// Create another student.
+		Student s2 = new Student("John", "Smith");
+		// Check that the ID number has gone up.
+		System.out.println("ID " + s2.getId());
+		// Check that the number of students has gone up.
+		System.out.println("Number of students " + Student.getStudentCount());
+	}
 }
