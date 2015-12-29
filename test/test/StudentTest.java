c1cf11c4e033113898e261e2788437294598467e
Anirudh <Anirudh@Anirudh-PC>
2015-12-28 23:02:45 -0500

    Added Comments

--- test/test/StudentTest.java
+++ test/test/StudentTest.java
@@ -12,6 +12,13 @@ public class StudentTest
 		student2 = new Student("Steve", "Nash");
 	}
 	
+	/**
+	 * testGetCurrentCr check to see if
+	 * the setCurrentEarnedCr and getCurrentEarnedCr
+	 * works correctly by seeing if the correct number
+	 * of credits is returned 
+	 */
+	
 	@Test
 	public void testGetCurrentEarnedCr()
 	{
@@ -19,6 +26,14 @@ public class StudentTest
 		student1.setCurrentEarnedCr(credits);
 		assertEquals(credits, student1.getCurrentEarnedCr());
 	}
+	
+	/**
+	 * testNotReadyToGraduate1 is used to
+	 * try to make sure that the readyToGraduate
+	 * method returns false if the requirements
+	 * are not met. In this case it returns false
+	 * because the LascComplete is false
+	 */
 	
 	@Test
 	public void testNotReadyToGraduate1()
@@ -30,12 +45,29 @@ public class StudentTest
 		assertFalse(student1.readyToGraduate());
 	}
 	
+	/**
+	 * testAnticipatedRemaingCredits is used to
+	 * validate if the correct number of remaining
+	 * credits is returned. You need 120 to graduate
+	 * and after completing 50 it validates to see
+	 * if 70 credits are needed to graduate
+	 */
+	
 	@Test
 	public void testAnticipatedRemaingCredits()
 	{
 		student1.setAnticipatedAdditionalCr(50);
 		assertEquals(70, student1.getAnticipatedRemainingCr());
 	}
+	
+	/**
+	 * testNotReadyToGraduate2 is used to
+	 * try to make sure that the readyToGraduate
+	 * method returns false if the requirements
+	 * are not met. In this case it returns false
+	 * because the earned credits does not meet
+	 * requirements
+	 */
 	
 	@Test
 	public void testNotReadyToGraduate2()
@@ -47,8 +79,16 @@ public class StudentTest
 		assertFalse(student2.readyToGraduate());
 	}
 	
+	/**
+	 * testReadyToGraduate is used to
+	 * try to make sure that the readyToGraduate
+	 * method returns true if the requirements
+	 * are met. In this case it returns true
+	 * because all the requirements are met
+	 */
+	
 	@Test
-	public void testReadyToGraduate2()
+	public void testReadyToGraduate()
 	{
 		student2.setCurrentEarnedCr(120);
 		student2.setGpa(4.8);
@@ -57,6 +97,14 @@ public class StudentTest
 		assertTrue(student2.readyToGraduate());
 	}
 	
+	/**
+	 * testGetCurrentRemainingCr is used to
+	 * validate if the correct number of remaining
+	 * credits is returned. You need 120 to graduate
+	 * and after completing 50 it validates to see
+	 * if 70 credits are needed to graduate
+	 */
+	
 	@Test
 	public void testGetCurrentRemainingCr()
 	{
@@ -64,12 +112,26 @@ public class StudentTest
 		assertEquals(70, student2.getCurrentRemainingCr());
 	}
 	
+	/**
+	 * testGetLascComplete is used to
+	 * check if the Lasc requirements
+	 * are met. Returns true if they 
+	 * are. Here returns true.
+	 */
+	
 	@Test
 	public void testGetLascComplete()
 	{
 		student2.setLascComplete(true);
 		assertTrue(student2.getLascComplete());
 	}
+	
+	/**
+	 * testGetLascComplete is used to
+	 * check if the Lasc requirements
+	 * are met. Returns true if they 
+	 * are. Here returns false.
+	 */
 	
 	@Test
 	public void testGetLascComplete2()
